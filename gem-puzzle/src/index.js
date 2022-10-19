24997738fb8ed154ca589a9ae2fc2f67b3263647
Larisa Volynkina <volynkinala@gmail.com>
2022-10-19 13:54:35 +0400

    feat: add timer, moves, winner message

--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -2,6 +2,69 @@ const buttonsWrapper = document.createElement('div');
 document.body.append(buttonsWrapper);
 buttonsWrapper.classList.add('buttons-wrapper');
 
+const infoWrapper = document.createElement('div');
+document.body.append(infoWrapper);
+infoWrapper.classList.add('info-wrapper');
+
+let movesCount = 0;
+
+const info = [
+  {
+    text: 'Time',
+    class: 'time',
+  },
+  {
+    text: 'Moves',
+    class: 'moves',
+  },
+];
+
+info.forEach((item) => {
+  const element = document.createElement('div');
+  element.classList.add('info-element');
+  infoWrapper.appendChild(element);
+  const paragraph = document.createElement('p');
+  paragraph.innerText = `${item.text}:`;
+  element.appendChild(paragraph);
+  const span = document.createElement('span');
+  paragraph.appendChild(span);
+  span.classList.add(item.class);
+});
+
+const movesSpan = document.querySelector('.moves');
+movesSpan.innerText = '0';
+const timeSpan = document.querySelector('.time');
+timeSpan.innerText = '00:00';
+
+const buttons = [
+  {
+    text: 'Restart',
+    class: 'restart',
+  },
+  {
+    text: 'Stop',
+    class: 'stop',
+  },
+  {
+    text: 'Save',
+    class: 'save',
+  },
+  {
+    text: 'Results',
+    class: 'result',
+  },
+];
+
+buttons.forEach((item) => {
+  const element = document.createElement('button');
+  buttonsWrapper.appendChild(element);
+  element.innerText = item.text;
+  element.classList.add('btn');
+  element.classList.add(item.class);
+});
+
+const stopBTN = document.querySelector('.stop');
+
 const soundIco = document.createElement('div');
 buttonsWrapper.append(soundIco);
 soundIco.classList.add('sound-ico');
@@ -13,13 +76,14 @@ playingField.classList.add('playing-field');
 
 // создание кнопок
 function mixTags(n) {
-  const arr = [];
-  for (let i = 0; arr.length < n; i += 1) {
-    const digit = Math.floor(Math.random() * n);
-    if (!arr.includes(digit)) {
-      arr.push(digit);
-    }
-  }
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 13, 14, 15];
+  // const arr = [];
+  // for (let i = 0; arr.length < n; i += 1) {
+  //   const digit = Math.floor(Math.random() * n);
+  //   if (!arr.includes(digit)) {
+  //     arr.push(digit);
+  //   }
+  // }
   return arr;
 }
 
@@ -111,10 +175,35 @@ function moveCurrentTag(tag) {
   }
 }
 
+function isWinner() {
+  const winMatrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 0],
+  ];
+  if (winMatrix.toString() === matrix.toString()) {
+    if (soundIco.classList.contains('active')) {
+      const winnerSound = new Audio('./assets/sound/winner.mp3');
+      winnerSound.autoplay = true;
+    }
+    const element = document.createElement('div');
+    document.body.append(element);
+    element.classList.add('finish-popup');
+    const paragraph = document.createElement('p');
+    element.append(paragraph);
+    paragraph.innerText = `Hooray! You solved the puzzle in ${timeSpan.textContent} and ${movesSpan.textContent} moves!`;
+    stopTimer();
+  }
+}
+
 playingField.addEventListener('click', (e) => {
   const tag = e.target;
   moveCurrentTag(tag);
   soundOn();
+  movesCount += 1;
+  movesSpan.innerText = movesCount;
+  isWinner();
 });
 
 playingField.addEventListener('transitionend', () => {
@@ -122,6 +211,7 @@ playingField.addEventListener('transitionend', () => {
     playingField.removeChild(playingField.lastChild);
   }
   createTagsHTML();
+  playingField.removeEventListener('click', startTimer);
 });
 
 // move sound
@@ -138,3 +228,36 @@ function soundOn() {
 soundIco.addEventListener('click', () => {
   soundIco.classList.toggle('active');
 });
+
+// clock
+
+let startTime = 0;
+let timer;
+
+function convertTimeToString() {
+  const current = startTime;
+  const date = new Date(current * 10);
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getSeconds();
+
+  const result = `${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}`;
+
+  timeSpan.textContent = result;
+}
+
+function startTimer() {
+  timer = setInterval(() => {
+    startTime += 1;
+
+    convertTimeToString();
+  }, 10);
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+stopBTN.addEventListener('click', stopTimer);
+playingField.addEventListener('click', startTimer);
